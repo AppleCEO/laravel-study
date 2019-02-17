ff3d04c7c5c2285006a55302b46c97d975953de4
kiljh <kiljh@me.com>
2019-02-17 11:54:43 +0900

    view and data binding

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,9 @@
 |
 */
 
-
-Route::get('/', [
-    'as' => 'home',
-    function () {
-        return '제 이름은 "home" 입니다.';
-    }
-]);
-
-Route::get('/home', function() {
-    return redirect(route('home'));
+Route::get('/', function() {
+    return view('welcome')->with([
+        'name' => 'Foo',
+        'greeting' => '안녕하세요?',
+    ]);
 });
